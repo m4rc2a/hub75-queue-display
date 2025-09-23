d7d0b180426dc6313936a6cb462cfb8c9c1a34e7
Marc Zander <m4rc2a.opensource@tutanota.de>
2025-09-23 10:49:39 +0000

    add HUB75 support and use better wifi name

--- wled00/const.h
+++ wled00/const.h
@@ -9,8 +9,8 @@
 
 //Defaults
 #define DEFAULT_CLIENT_SSID "Your_Network"
-#define DEFAULT_AP_SSID     "WLED-AP"
-#define DEFAULT_AP_PASS     "wled1234"
+#define DEFAULT_AP_SSID     "Matrix-WLED"
+#define DEFAULT_AP_PASS     "morpheus"
 #define DEFAULT_OTA_PASS    "wledota"
 #define DEFAULT_MDNS_NAME   "x"
 
